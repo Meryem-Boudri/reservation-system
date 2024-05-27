814eb19ea662ea89e1d89400d027305221c0eba1
Meryem-Boudri <boudrimeryem11@gmail.com>
2024-05-27 20:08:03 +0000

    first commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from "@angular/router";
 import {NavbarComponent} from "../navbar/navbar.component";
+import { Router } from '@angular/router';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-dashboard',
@@ -8,11 +11,39 @@ import {NavbarComponent} from "../navbar/navbar.component";
   imports: [
     RouterLink,
     RouterOutlet,
-    NavbarComponent
+    NavbarComponent,
+    HttpClientModule,
+    NgIf,
+    NgForOf
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  sessions: any[] = [];
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loadSessions();
+  }
+
+  loadSessions() {
+    this.http.get<any[]>(`http://localhost:3000/sessions`).subscribe((sessions: any[]) => {
+      this.sessions = sessions;
+      console.log(sessions);
+    });
+  }
+
+  reserveSession(session: any) {
+    // Logique pour réserver une séance
+    console.log(`Réserver la séance : ${session.name}`);
+  }
+
+  cancelReservation(session: any) {
+    // Logique pour annuler une réservation
+    console.log(`Annuler la réservation pour la séance : ${session.name}`);
+  }
+
 
 }
